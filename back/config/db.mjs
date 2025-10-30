2be9b716f2cbaefbcab9b82931ea926e9ad04765
Gino Gregoret <gregoretgino@gmail.com>
2025-10-30 20:14:01 -0300

    prueba

--- back/config/db.mjs
+++ back/config/db.mjs
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-    process.env.NAME_DB,
+    "ventas",
+    "user",
+    "password",
     {
-        host: process.env.HOST_DB,
-        port: process.env.PORT_DB,
-        dialect: process.env.DIALECT_DB
-    }
+        dialect:'mysql',
+        host:'localhost',
+        port: 3306
+    }   
 )
